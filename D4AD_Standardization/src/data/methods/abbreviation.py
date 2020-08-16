f9e395369fb76509a2267595a6a6a8c643a2a381
Kwame Porter Robinson <kporterrobinson@gmail.com>
2020-08-16 16:30:38 -0400

    (WIP) Set up match rule set, verified against 100 random rows, looks like 88 - 97 % accuracy, good enought to go forward. Enables use to generate spans from which we have unstandardized contnet that we can review, create a list of standardized items (keep on going until nothing new?)
    Then can iteratively label everything with Prodigy and have a matched data set of Prequisties

--- D4AD_Standardization/src/data/methods/abbreviation.py
+++ D4AD_Standardization/src/data/methods/abbreviation.py
@@ -2,25 +2,6 @@ import spacy
 import pandas as pd
 from spacy.tokenizer import Tokenizer
 
-#  from https://spacy.io/usage/linguistic-features#native-tokenizers
-special_cases = {":)": [{"ORTH": ":)"}]}  # this keeps this string no matter what
-prefix_re = re.compile(r'''^[[("']''') # this is regex for keeping prefixes in a string
-suffix_re = re.compile(r'''[])"']$''') # ^ but for suffices
-infix_re = re.compile(r'''[-~]''') # this keeps these characters
-simple_url_re = re.compile(r'''^https?://''') # this is annoying
-
-# build a custom tokenizer
-#
-# we want to build a tokenizer that:
-#   splits on stopwords with certain dependencies  (like to but not Abilty To Benefit)
-#   splits on commas, word like and
-def custom_tokenizer(nlp):
-    return Tokenizer(nlp.vocab, rules=special_cases,
-                                prefix_search=prefix_re.search,
-                                suffix_search=suffix_re.search,
-                                infix_finditer=infix_re.finditer,
-                                url_match=simple_url_re.match)
-
 # todo: set as env variable for raw main table
 filepath = "./data/raw/etpl_all_programsJune3.xls"
 
@@ -32,12 +13,55 @@ columns = [
     "FEATURESDESCRIPTION"
 ]
 
+random_state = 42
+N = 100
 df = pd.read_excel(filepath, usecols=columns)
-small_df = df.sample(n=100, random_state=42)
+small_df = df.sample(n=N, random_state=random_state)
 #  need to suss this out but basically, need a giant list of words,
 # ONET tech list, (maybe even skills), Credential Engine Competency framework(s)
 # (https://credentialfinder.org/ , select frameworks)
 
-nlp = spacy.load("en_core_web_lg")
+# random_state, N patterns
+patterns =\
+    [
+        [ # these break up small_df.iloc[0] into unstandardized tokens
+            #[{'POS': 'PUNCT'}],
+            [{'POS': 'CCONJ'}],
+        ],
+        [ # modifiction that breaks up small_df.iloc[1]
+            #[{'POS': 'PUNCT'}, {'POS': 'PROPN', 'OP': '!'}]           
+        ],
+        [ # modifiction that breaks up small_df.iloc[7]
+            [{'ORTH': '/'}]
+        ],
+        [ # modifiction that comnbines small_df.iloc[15], [1]
+         # this should **rewrite** the rules for 0a, 1 above
+            [{'ORTH': ','}]
+        ],
+        [ # modifiction seen generally past 50 or os
+            [{'ORTH': ';'}]
+        ],
+    ]
 
-nlp.tokenizer = custom_tokenizer(nlp)
+
+nlp = spacy.load("en_core_web_lg")
+matcher = Matcher(nlp.vocab)
+
+matcher.add("DoNotStandardizeContent",
+            [
+                {'POS': 'CCONJ'},
+                {'ORTH': '/'},
+                {'ORTH': ','},
+                {'ORTH': ';'}]
+            ]
+)
+
+doc = nlp("HELLO WORLD on Google Maps.")  # Can we get away with POS, ORTH tagger only
+# todo: 
+matches = matcher(doc)
+
+# Matcher call
+# https://spacy.io/api/matcher#call
+# A list of (match_id, start, end) tuples, describing the matches. A match tuple describes a span doc[start:end]. The match_id is the ID of the added match pattern.
+
+the span, start, end tup
