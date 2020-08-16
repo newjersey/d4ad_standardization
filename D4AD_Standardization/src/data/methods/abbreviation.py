d691ff13ffdc18aec697d5f29d0217c67b70e35d
Kwame Porter Robinson <kporterrobinson@gmail.com>
2020-08-16 10:02:11 -0400

    (notes) I think I identified a principal and actually fun way to identify non-standardized content with spacy matcher rule tokenization, should be reliably from bionomial proportion statistics (e.g. all 100 check out from a random sample, should be mega-good)
    next steps should be to try this out.Since i'm really just tryign split on , conj things and skip certain kinds of uses of & (?)
    this should be feasible. So, like a rule matcher that gets 80% and then a sstatistical matcher within that.
    Note that you might no need full DEP parasing, there are lighter forms of generating DOCs

--- D4AD_Standardization/src/data/methods/abbreviation.py
+++ D4AD_Standardization/src/data/methods/abbreviation.py
@@ -1,4 +1,25 @@
+import spacy
 import pandas as pd
+from spacy.tokenizer import Tokenizer
+
+#  from https://spacy.io/usage/linguistic-features#native-tokenizers
+special_cases = {":)": [{"ORTH": ":)"}]}  # this keeps this string no matter what
+prefix_re = re.compile(r'''^[[("']''') # this is regex for keeping prefixes in a string
+suffix_re = re.compile(r'''[])"']$''') # ^ but for suffices
+infix_re = re.compile(r'''[-~]''') # this keeps these characters
+simple_url_re = re.compile(r'''^https?://''') # this is annoying
+
+# build a custom tokenizer
+#
+# we want to build a tokenizer that:
+#   splits on stopwords with certain dependencies  (like to but not Abilty To Benefit)
+#   splits on commas, word like and
+def custom_tokenizer(nlp):
+    return Tokenizer(nlp.vocab, rules=special_cases,
+                                prefix_search=prefix_re.search,
+                                suffix_search=suffix_re.search,
+                                infix_finditer=infix_re.finditer,
+                                url_match=simple_url_re.match)
 
 # todo: set as env variable for raw main table
 filepath = "./data/raw/etpl_all_programsJune3.xls"
@@ -12,7 +33,11 @@ columns = [
 ]
 
 df = pd.read_excel(filepath, usecols=columns)
-
+small_df = df.sample(n=100, random_state=42)
 #  need to suss this out but basically, need a giant list of words,
 # ONET tech list, (maybe even skills), Credential Engine Competency framework(s)
 # (https://credentialfinder.org/ , select frameworks)
+
+nlp = spacy.load("en_core_web_lg")
+
+nlp.tokenizer = custom_tokenizer(nlp)
