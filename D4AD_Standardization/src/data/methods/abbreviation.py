62d3d84748545951a2db92c9a6644e72f72a986f
Kwame Porter Robinson <kporterrobinson@gmail.com>
2020-08-17 08:55:10 -0400

    (fix) Simplifies spacy API use for matcher, gives name to pattern, includes note on using pipe to pass over iterable (migth want to make a yeildable for that)

--- D4AD_Standardization/src/data/methods/abbreviation.py
+++ D4AD_Standardization/src/data/methods/abbreviation.py
@@ -23,34 +23,25 @@ small_df = df.sample(n=N, random_state=random_state)
 # ONET tech list, (maybe even skills), Credential Engine Competency framework(s)
 # (https://credentialfinder.org/ , select frameworks)
 
-# random_state, N patterns
+# Abrreviation unstandardrandom_state, N patterns
 patterns =\
     [
         # these break up small_df.iloc[0] into unstandardized tokens
-        #[{'POS': 'PUNCT'}],
-        ("A", [{'POS': 'CCONJ'}]),
+        #[{'POS': 'PUNCT'}],  # fails in later samples
+        [{'POS': 'CCONJ'}],
         # modifiction that breaks up small_df.iloc[7]
-        ("B", [{'ORTH': '/'}]),
-        # modifiction that comnbines small_df.iloc[15], [1]
-        # this should **rewrite** the rules for 0a, 1 above
-        ("C", [{'ORTH': ','}]),
+        [{'ORTH': '/'}],
+        # modifiction that combines small_df.iloc[15], [1]
+        [{'ORTH': ','}],
         # modifiction seen generally past 50 or os
-        ("D", [{'ORTH': ';'}])
+        [{'ORTH': ';'}]
     ]
 
 matcher = Matcher(nlp.vocab)
-
-# note: the signature suggests that patterns
-
-for pattern_name, pattern in patterns:
-    matcher.add(pattern_name, None, pattern)
+matcher.add("DoNotStandardize", patterns)
 
 doc = nlp(small_df.iloc[0].PREREQUISITES)
-
-#doc = nlp("HELLO WORLD on Google Maps.")  # Can we get away with POS, ORTH tagger only
-# "H.S. diploma or GED. If not must pass the Wonderlic test."
 matches = matcher(doc) #  [(14862748245026736845, 2, 3)] --> [or]
 
-# Matcher call
-# https://spacy.io/api/matcher#call
-# A list of (match_id, start, end) tuples, describing the matches. A match tuple describes a span doc[start:end]. The match_id is the ID of the added match pattern.
+# see: matcher.pipe
+# at https://spacy.io/api/matcher#pipe
