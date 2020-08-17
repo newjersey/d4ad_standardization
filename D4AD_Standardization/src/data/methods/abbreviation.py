cdf134215ada0e4364ec06942914d85c5cf2005c
Kwame Porter Robinson <kporterrobinson@gmail.com>
2020-08-16 22:57:20 -0400

    (WIP/feat) Adds match list for tokenizing on content that does not need to be standardized Consider adding "." that are sentence endings? Not sure if possible
    todo: see if we can use * to add lists, run over docs with pipe, over the random sample
    inspect list for mapping trends. Probably need to launch Prodigy at that point?

--- D4AD_Standardization/src/data/methods/abbreviation.py
+++ D4AD_Standardization/src/data/methods/abbreviation.py
@@ -1,6 +1,8 @@
 import spacy
 import pandas as pd
-from spacy.tokenizer import Tokenizer
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_lg")
 
 # todo: set as env variable for raw main table
 filepath = "./data/raw/etpl_all_programsJune3.xls"
@@ -24,44 +26,31 @@ small_df = df.sample(n=N, random_state=random_state)
 # random_state, N patterns
 patterns =\
     [
-        [ # these break up small_df.iloc[0] into unstandardized tokens
-            #[{'POS': 'PUNCT'}],
-            [{'POS': 'CCONJ'}],
-        ],
-        [ # modifiction that breaks up small_df.iloc[1]
-            #[{'POS': 'PUNCT'}, {'POS': 'PROPN', 'OP': '!'}]           
-        ],
-        [ # modifiction that breaks up small_df.iloc[7]
-            [{'ORTH': '/'}]
-        ],
-        [ # modifiction that comnbines small_df.iloc[15], [1]
-         # this should **rewrite** the rules for 0a, 1 above
-            [{'ORTH': ','}]
-        ],
-        [ # modifiction seen generally past 50 or os
-            [{'ORTH': ';'}]
-        ],
+        # these break up small_df.iloc[0] into unstandardized tokens
+        #[{'POS': 'PUNCT'}],
+        ("A", [{'POS': 'CCONJ'}]),
+        # modifiction that breaks up small_df.iloc[7]
+        ("B", [{'ORTH': '/'}]),
+        # modifiction that comnbines small_df.iloc[15], [1]
+        # this should **rewrite** the rules for 0a, 1 above
+        ("C", [{'ORTH': ','}]),
+        # modifiction seen generally past 50 or os
+        ("D", [{'ORTH': ';'}])
     ]
 
-
-nlp = spacy.load("en_core_web_lg")
 matcher = Matcher(nlp.vocab)
 
-matcher.add("DoNotStandardizeContent",
-            [
-                {'POS': 'CCONJ'},
-                {'ORTH': '/'},
-                {'ORTH': ','},
-                {'ORTH': ';'}]
-            ]
-)
+# note: the signature suggests that patterns
 
-doc = nlp("HELLO WORLD on Google Maps.")  # Can we get away with POS, ORTH tagger only
-# todo: 
-matches = matcher(doc)
+for pattern_name, pattern in patterns:
+    matcher.add(pattern_name, None, pattern)
+
+doc = nlp(small_df.iloc[0].PREREQUISITES)
+
+#doc = nlp("HELLO WORLD on Google Maps.")  # Can we get away with POS, ORTH tagger only
+# "H.S. diploma or GED. If not must pass the Wonderlic test."
+matches = matcher(doc) #  [(14862748245026736845, 2, 3)] --> [or]
 
 # Matcher call
 # https://spacy.io/api/matcher#call
 # A list of (match_id, start, end) tuples, describing the matches. A match tuple describes a span doc[start:end]. The match_id is the ID of the added match pattern.
-
-the span, start, end tup
